f879c628ef73fca765097bacfc9d2dc0e548d67d
omarmagdy217 <36456356+omarmagdy217@users.noreply.github.com>
2019-05-09 02:22:40 +0200

    Update Memory.cs

--- Main_Code/Memory.cs
+++ Main_Code/Memory.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Best_Fit
+namespace Memory_Fit
 {
     struct Hole
     {
